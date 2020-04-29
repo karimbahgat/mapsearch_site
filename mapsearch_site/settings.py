c45291a3bbad5d1c638cdf5f7953842fc7afa272
Karim Bahgat <karim.bahgat.norway@gmail.com>
2020-04-30 01:08:05 +0200

    Basic map of image layout
    Requires fixing djgeojsona and leaflet geos bugs

--- mapsearch_site/settings.py
+++ mapsearch_site/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'mapsearch.apps.MapsearchConfig',
 
     # dependencies
-    #'djgeojson',
-    #'leaflet',
+    'djgeojson',
+    'leaflet',
 ]
 
 #GDAL_LIBRARY_PATH = 'fjdslkfjs'
