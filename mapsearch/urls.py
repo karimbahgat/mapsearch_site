993fef7abc9ec2913a87865bff4126539dd49d61
Karim Bahgat <karim.bahgat.norway@gmail.com>
2020-04-29 18:36:46 +0200

    Store image thumbnail and display

--- mapsearch/urls.py
+++ mapsearch/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.search, name='home'),
     path('', views.search, name='search'),
+    path('map/add/', views.map_add, name='map_add'),
+    path('map/update/<int:pk>/about', views.map_update_about, name='map_update_about'),
     path('map/view/<int:pk>/', views.map_view, name='map_view'),
     path('map/view/<int:pk>/<str:tab>', views.map_view, name='map_view'),
+
     path('map/georef/<int:pk>/', views.map_auto_georef, name='map_auto_georef'),
 ]
--- mapsearch/views.py
+++ mapsearch/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.http import QueryDict
 
 from .models import Map
 from .forms import MapForm
+
+import urllib
+import io
+import base64
+from PIL import Image
 
 # Create your views here.
 
 def search(request):
     if request.GET:
+        # get search results
         dct = request.GET.dict()
         
         if dct['search'].startswith('http'):
@@ -16,17 +23,9 @@ def search(request):
             matches = Map.objects.filter(url=dct['search'])
             mapp = matches[0] if matches else None
             if mapp is None:
-                # download image
-                # ...
-
-                # create thumbnail
-                # ...
-                
-                # add to db
-                mapp = Map.objects.create(url=dct['search'])
-                
-                # process map in background
-                # ...
+                request.GET = QueryDict(mutable=True)
+                request.GET['url'] = dct['search']
+                return map_add(request)
                 
             # link to the map view
             return redirect('map_view', mapp.pk)
@@ -36,13 +35,87 @@ def search(request):
             # ...
             return render(request, 'templates/search.html', {})
     else:
-        return render(request, 'templates/search.html', {})
+        # front page search screen
+        recent_maps = []
+        for m in Map.objects.order_by('-created'): # limit somehow...
+            if m.thumbnail:
+                print(repr(m.thumbnail))
+                thumb = 'data:image/png;base64,' + str(m.thumbnail, 'ascii')
+                print(repr(thumb))
+            else:
+                thumb = None
+            recent_maps.append({'obj':m, 'thumb':thumb})
+        return render(request, 'templates/search.html', {'recent_maps':recent_maps})
+
+# MAP
+
+def map_add(request):
+    dct = request.GET.dict()
+    
+    # download image
+    fobj = io.BytesIO(urllib.request.urlopen(dct['url']).read())
+    img = Image.open(fobj)
+    print(img)
+    
+    # add to db
+    mapp = Map.objects.create(url=dct['url'])
+
+    # create thumbnail
+    max_size = 150
+    longest = max(img.size)
+    scale = max_size / float(longest)
+    size = img.size[0] * scale, img.size[1] * scale
+    img.thumbnail(size)
+    print(img)
+
+    fobj = io.BytesIO()
+    img.save(fobj, "PNG")
+    raw = base64.b64encode(fobj.getvalue())
+    mapp.thumbnail = raw
+    
+    # process map in background? 
+    # ...
+
+    # save
+    mapp.save()
+
+    # redirect to map view
+    return redirect('map_view', mapp.pk)
+
+def map_update_about(request, pk):
+    # add to db
+    mapp = Map.objects.get(pk=pk)
+
+    # download image
+    fobj = io.BytesIO(urllib.request.urlopen(mapp.url).read())
+    img = Image.open(fobj)
+    print(img)
+
+    # create thumbnail
+    max_size = 150
+    longest = max(img.size)
+    scale = max_size / float(longest)
+    size = img.size[0] * scale, img.size[1] * scale
+    img.thumbnail(size)
+    print(img)
+
+    fobj = io.BytesIO()
+    img.save(fobj, "PNG")
+    raw = base64.b64encode(fobj.getvalue())
+    mapp.thumbnail = raw
+
+    # save
+    mapp.save()
+
+    # redirect to map view
+    return redirect('map_view', mapp.pk)
 
 def map_view(request, pk, tab=None):
     mapp = Map.objects.get(pk=pk)
     mappform = MapForm(instance=mapp)
     tab = tab or 'about'
     return render(request, 'templates/map_view_{}.html'.format(tab), {'map':mapp, 'form':mappform, 'tab':tab})
+
 
 def map_auto_georef(request, pk):
     import subprocess
